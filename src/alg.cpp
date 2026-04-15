411c4e4ba3b43e0f3ea24136761f362ecf037748
Vaor22 <96342252+Vaor22@users.noreply.github.com>
2026-04-15 22:09:36 +0300

    Add infx2pstfx and eval functions
    Implemented infix to postfix conversion and evaluation functions.

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,92 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int getPriority(char op) {
+  if (op == '+' || op == '-') return 1;
+  if (op == '*' || op == '/') return 2;
+  return 0;
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+bool isDigit(char ch) {
+  return ch >= '0' && ch <= '9';
+}
+
+std::string infx2pstfx(const std::string& inf) {
+  TStack<char, 100> stack;
+  std::string postfix = "";
+
+  for (size_t i = 0; i < inf.length(); i++) {
+    char ch = inf[i];
+
+    if (ch == ' ') continue;
+
+    if (isDigit(ch)) {
+      while (i < inf.length() && isDigit(inf[i])) {
+        postfix += inf[i];
+        i++;
+      }
+      postfix += ' ';
+      i--;
+    } else if (ch == '(') {
+      stack.push(ch);
+    } else if (ch == ')') {
+      while (!stack.isEmpty() && stack.get() != '(') {
+        postfix += stack.pop();
+        postfix += ' ';
+      }
+      if (!stack.isEmpty() && stack.get() == '(') {
+        stack.pop();
+      }
+    } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+      while (!stack.isEmpty() && stack.get() != '(' &&
+             getPriority(stack.get()) >= getPriority(ch)) {
+        postfix += stack.pop();
+        postfix += ' ';
+      }
+      stack.push(ch);
+    }
+  }
+
+  while (!stack.isEmpty()) {
+    postfix += stack.pop();
+    postfix += ' ';
+  }
+
+  if (!postfix.empty() && postfix.back() == ' ') {
+    postfix.pop_back();
+  }
+
+  return postfix;
+}
+
+int eval(const std::string& post) {
+  TStack<int, 100> stack;
+
+  for (size_t i = 0; i < post.length(); i++) {
+    char ch = post[i];
+
+    if (ch == ' ') continue;
+
+    if (isDigit(ch)) {
+      int num = 0;
+      while (i < post.length() && isDigit(post[i])) {
+        num = num * 10 + (post[i] - '0');
+        i++;
+      }
+      stack.push(num);
+      i--;
+    } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+      int val2 = stack.pop();
+      int val1 = stack.pop();
+
+      switch (ch) {
+        case '+': stack.push(val1 + val2); break;
+        case '-': stack.push(val1 - val2); break;
+        case '*': stack.push(val1 * val2); break;
+        case '/': stack.push(val1 / val2); break;
+      }
+    }
+  }
+
+  return stack.get();
 }
